555030dad9f63533e77b4d19bc933a06e9b37b30
wadhass <ahmedhassansumu@gmail.com>
2025-03-25 17:51:15 +0200

    erroe added

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -80,10 +80,7 @@ export const stripeWebhooks = async (request, response) => {
     try {
         event = Stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
     } catch (error) {
-        catch (error) {
-            response.status(400).send(`Webhook error: ${error.message}`); // ✅
-          }
-          
+        response.status(400).send(`Webhook error: ${error.message}`);
     }
 
     // Handle the event 
