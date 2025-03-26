5583dea6f44a688be79b1a6bff57d64df37543de
wadhass <ahmedhassansumu@gmail.com>
2025-03-26 12:15:37 +0200

    mongodb corrected

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -18,14 +18,17 @@ import mongoose from 'mongoose';
 // Connect to the MongoDB database
 const connectDB = async () => {
   try {
+    // Check if the MONGODB_URL environment variable is defined
+    if (!process.env.MONGODB_URL) {
+      console.error('MongoDB connection string is missing. Please define MONGODB_URL in your environment variables.');
+      process.exit(1); // Exit the process if MONGODB_URL is not defined
+    }
+
     // Connect to MongoDB using the connection string from environment variables
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,    // Ensures MongoDB driver uses the latest URL parser
-      useUnifiedTopology: true, // Ensures MongoDB driver uses the new topology engine
-    });
+    await mongoose.connect(process.env.MONGODB_URL);
 
     // Log successful connection
-    console.log('Database connected');
+    console.log('Database connected successfully');
   } catch (error) {
     // Log error if connection fails
     console.error('Error connecting to MongoDB:', error.message);
@@ -34,3 +37,4 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
